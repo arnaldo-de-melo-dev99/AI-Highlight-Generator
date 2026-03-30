d82a5ae2321316c2243129e878d688c4ea76f629
ade-melo <arnaldodemlo99@gmail.com>
2026-03-30 18:32:44 +0100

    feate to add crud(function)db

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,5 +1,6 @@
 import { client, db } from ".";
 import { goals, goalCompletions } from "./schema";
+import dayjs from "dayjs";
 
 async function seed() {
     await db.delete(goalCompletions)
@@ -11,9 +12,11 @@ async function seed() {
         {title: 'Meditar', desiredWeeklyFrequency: 1},
     ]).returning();
 
+    const startOfWeek = dayjs().startOf('week');
+
     await db.insert(goalCompletions).values([
-        {goalId: result[0].id, completedAt: new Date()},
-        {goalId: result[1].id, completedAt: new Date()}
+        {goalId: result[0].id, completedAt: startOfWeek.toDate()},
+        {goalId: result[1].id, completedAt: startOfWeek.add(1, 'day').toDate()},
     ]);
 }
 
