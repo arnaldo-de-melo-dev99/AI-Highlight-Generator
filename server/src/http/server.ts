01f696bff244a6dae5351d74bdaa52471ce991d2
ade-melo <arnaldodemlo99@gmail.com>
2026-03-30 17:54:48 +0100

    feat install some dependencies

---
+++ server/src/http/server.ts
@@ -0,0 +1,10 @@
+//import "biome" from "@biomejs/biome";
+import fastify from "fastify";
+
+const app = fastify();
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log("Server is running on http://localhost:3333");
+})
