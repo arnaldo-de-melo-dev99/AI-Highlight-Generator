b5f137d3c659f0b9a74cd8ff17172a923b54fb95
ade-melo <arnaldodemlo99@gmail.com>
2026-05-08 19:16:59 +0100

    feat: finish post route(finished using fastfy)

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,21 +1,30 @@
 //import "biome" from "@biomejs/biome";
 import fastify from "fastify";
+import { 
+    serializerCompiler, 
+    validatorCompiler, 
+    type ZodTypeProvider } from 'fastify-type-provider-zod';
 import { createGoal } from "../functions/create-goal";
 import z from "zod";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.post('/goals', async request => {
-    const createGoalBodySchema = z.object({
-        title: z.string(),
-        desiredWeeklyFrequency: z.number().int().min(1).max(7),
-    });
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-    const body = createGoalBodySchema.parse(request.body);
-
+app.post('/goals', {
+    schema: {
+        body: z.object({
+            title: z.string(),
+            desiredWeeklyFrequency: z.number().int().min(1).max(7),
+        })
+    }
+} , 
+async request => {
+    const { title, desiredWeeklyFrequency } = request.body;
    await createGoal({
-        title: body.title,
-        desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+        title,
+        desiredWeeklyFrequency,
     });
 });
 
