fde3e8af6cb5f1bf2ebd823fdc4ee9ba48e9eeeb
ade-melo <arnaldodemlo99@gmail.com>
2026-05-08 19:51:18 +0100

    feat: getWeekPendingGoals

--- server/src/functions/get-week-pending-goals.ts
+++ server/src/functions/get-week-pending-goals.ts
@@ -1,3 +1,30 @@
-export function getWeekPendingGoals() {
+import dayjs from "dayjs";
+import weekOfYear from "dayjs/plugin/weekOfYear";
+import { db } from "../db";
+import { goalCompletions, goals } from "../db/schema";
+import {and, sql, lte} from "drizzle-orm";
+import { count } from "console";
 
+dayjs.extend(weekOfYear);
+
+export function getWeekPendingGoals() {
+    const lastDayOfWeek = dayjs().endOf('week').toDate();
+
+    const goalsCreaedUpToWeek = db.$with('goals_created_up_to_week').as(
+        db.select({
+            ids: goals.id,
+            title: goals.title,
+            desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+            createdAt: goals.createdAt,
+        })
+        .from(goals)
+        .where(lte(goals.createdAt, lastDayOfWeek))
+    );
+
+    const goalCompletionCounts = db.$with('goal_completion_counts').as(
+        db.select({
+            goalId: goalCompletions.goalId,
+            completionCount: sql`count(${goalCompletions.id})`
+        }).from(goalCompletions).groupBy(goalCompletions.goalId)
+    )
 }
