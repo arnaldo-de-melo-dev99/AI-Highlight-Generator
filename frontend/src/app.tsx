66ff1739c699c5c0a6c26465bb052641c940363d
ade-melo <arnaldodemlo99@gmail.com>
2026-05-25 18:15:17 +0100

    feat: install tailwind=>dev-dependecie

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,8 +1,17 @@
+import logo from './assets/background.svg'
+import letStart from './assets/lets-start-ilustration.svg'
+
 export function App() {
 
   return (
     <>
-      <h1>Started Front</h1>
+      <div className="h-screen flex flex-col items-center justify-center gap-8 fixed">
+        <img src={logo} className="w-1/2 h-1/2" alt="GoWeek" />
+        <img src={letStart} className="" alt="Let's Start" />
+        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+          Não perca mais tempo, organize sua semana de forma eficiente e aproveite ao máximo cada dia com o GoWeek!
+        </p>
+      </div>
     </>
   )
 }
