d3ee7c40fe5063d3ac5d9e090b85ac6ae426a624
ade-melo <arnaldodemlo99@gmail.com>
2026-05-25 18:43:32 +0100

    create new svg=>logos

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,13 +1,13 @@
 import logo from './assets/background.svg'
-import letStart from './assets/lets-start-ilustration.svg'
+import letStart from './assets/logo.svg'
 
 export function App() {
 
   return (
     <>
       <div className="h-screen flex flex-col items-center justify-center gap-8">
-        <img src={logo} className="w-1/2 h-1/2" alt="GoWeek" />
         <img src={letStart} className="" alt="Let's Start" />
+        <img src={logo} className="w-1/2 h-1/2" alt="GoWeek" />
         <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
           Não perca mais tempo, organize sua semana de forma eficiente e aproveite ao máximo cada dia com o GoWeek!
         </p>
