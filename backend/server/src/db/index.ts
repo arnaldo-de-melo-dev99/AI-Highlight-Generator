35c765330a7b7ce6580aec6a6cccf3781fcec561
ade-melo <arnaldodemlo99@gmail.com>
2026-05-25 13:16:27 +0100

    refactor: removed backend folder

--- backend/server/src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schema";
-import { env }  from '../env';
-
-const client = postgres(env.DATABASE_URL);
-const db = drizzle(client, { schema, logger: true });
-
-export { client, db };
